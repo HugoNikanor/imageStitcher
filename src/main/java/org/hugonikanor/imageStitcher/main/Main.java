a25116e5261e0a7fe0111818523e3a2dae583f63
Hugo <hugo.hornquist@gmail.com>
2016-04-06 17:07:40 +0200

    main method updated slightly to represent final usage

--- src/main/java/org/hugonikanor/imageStitcher/main/Main.java
+++ src/main/java/org/hugonikanor/imageStitcher/main/Main.java
@@ -1,12 +1,26 @@
 package org.hugonikanor.imageStitcher.main;
 
-import org.hugonikanor.imageStitcher.math.GreatestFactors;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import org.hugonikanor.imageStitcher.fileReader.FileSystemAccess;
+import org.hugonikanor.imageStitcher.imageCreator.ImageStitcher;
 
 public class Main {
 	public static void main( String[] args ) {
-		for( int i = 1; i <= 20; i++ ) {
-			int[] factors = GreatestFactors.Get( i );
-			System.out.printf( "%02d: %02d x %d%n", i, factors[0], factors[1] );
+		
+		if( args.length < 0 ) {
+			System.out.println( "Please enter a directory path" );
 		}
+
+		FileSystemAccess fsa = new FileSystemAccess( new File(args[0]) );
+
+		ImageStitcher is = new ImageStitcher( 
+				(BufferedImage[]) fsa.getImages().toArray() );
+
+		//BufferedImage stitchedImage = is.getStitchedImage();
+
+		//fsa.writeImage( stitchedImage, "outputImage" );
+
 	}
 }
